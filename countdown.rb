2893404f8c4a97749c2aa85b1a305d9051b83b83
Luiz Pereira <luizfper@gmail.com>
2019-01-28 22:10:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown (times)
+  counter = times
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -=1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (times)
+  counter = times
+  while counter >0
+    puts "#{counter} SECOND(S)!"
+    curtime = Time.now
+    while Time.now<curtime+2
+    end
+    counter -=1
+  end
+  "HAPPY NEW YEAR!"
+
+  end
